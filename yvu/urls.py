3bd483e950802fa6bc65f7f927491a52a7c546c2
Bandi Naveenkumar <stylishnaveen17@gmail.com>
2022-01-25 20:07:51 +0530

    Initial commit

---
+++ yvu/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from yvu import views
+
+urlpatterns=[
+    path('',views.mcafrds)
+]
---
+++ yvu/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse
+
+
+# Create your views here.
+def mcafrds(request):
+    return HttpResponse('mca batch 2018 to 2021 yogi vemana university college')
